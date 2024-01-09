980fe9e8858771d0aa997c2e8d3d47a80ede9b4a
shridharrai <shridharrai3@gmail.com>
2024-01-09 06:54:33 +0530

    4th day

---
+++ Basics/CountDigits.java
@@ -0,0 +1,17 @@
+ import java.util.Scanner;
+
+ public class CountDigits {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        int count = 0;
+        while(n > 0) {
+            count++;
+            n = n / 10;
+        }
+
+        System.out.println(count);
+        scn.close();
+    }
+ }
